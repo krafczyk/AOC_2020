2ad832fe7571bd19e32d0269b1f5dfbae63d4664
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-12-06 09:19:30 -0600

    Solve Day 6 task 2

--- day_6.py
+++ day_6.py
@@ -21,13 +21,24 @@ with open(input_filepath, 'r') as f:
     group_forms = f.read().split('\n\n')
 
 sum_1 = 0
+sum_2 = 0
 for group_form in group_forms:
     counts = {}
+    # Count number of group members
+    num_in_group = len(list(filter(lambda s: s!="",group_form.split('\n'))))
     forms = group_form.replace('\n', '')
     for i in range(len(forms)):
         let = forms[i]
         counts[let] = counts.get(let, 0)+1
 
+    num_all_answered = 0
+    for let in counts:
+        if counts[let] == num_in_group:
+            num_all_answered += 1
+
+    sum_2 += num_all_answered
+
     sum_1 += len(counts.keys())
 
 print(f"Day 6 task 1: {sum_1}")
+print(f"Day 6 task 2: {sum_2}")
