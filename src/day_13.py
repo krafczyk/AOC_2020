bab35532b1ffeb7040fb5cce1ed5d4bc20e30609
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-12-13 16:06:25 -0600

    Solve day 13 task 1

--- src/day_13.py
+++ src/day_13.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import numpy as np
 
 # Define Argument Parser
 parser = argparse.ArgumentParser('Solves Advent of Code Day 13')
@@ -19,3 +20,13 @@ if not os.path.exists(input_filepath):
 
 with open(input_filepath, 'r') as f:
     lines = f.readlines()
+
+earliest_timestamp = int(lines[0].strip())
+
+bus_list = list(map(lambda s: int(s), filter(lambda s: s != 'x', lines[1].strip().split(','))))
+
+bus_nums = np.array(bus_list)
+time_to_next = bus_nums*((earliest_timestamp//bus_nums)+1)-earliest_timestamp
+i_bus = np.argmin(time_to_next, axis=0)
+
+print(f"Day 13 task 1: {bus_nums[i_bus]*time_to_next[i_bus]}")
