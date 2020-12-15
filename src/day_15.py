77ae5810f217e094c509e9f78e3d81cc09d86665
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-12-15 04:36:42 -0600

    Solve day 15 task 1

--- src/day_15.py
+++ src/day_15.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import numpy as np
 
 # Define Argument Parser
 parser = argparse.ArgumentParser('Solves Advent of Code Day 15')
@@ -19,3 +20,17 @@ if not os.path.exists(input_filepath):
 
 with open(input_filepath, 'r') as f:
     lines = f.readlines()
+
+nums = list(map(lambda s: int(s), lines[0].split(',')))
+
+nums_array = np.array(nums)
+
+while len(nums) < 2020:
+    last_where = np.where(nums_array == nums_array[-1])[0]
+    if len(last_where) == 1:
+        nums.append(0)
+    else:
+        nums.append(last_where[-1]-last_where[-2])
+    nums_array = np.array(nums)
+
+print(f"Day 15 task 1: {nums_array[-1]}")
