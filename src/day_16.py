8b72ec8ac5ab265a6ca606598357104be789f7ae
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-12-16 09:22:58 -0600

    Solve day 16 task 2!

--- src/day_16.py
+++ src/day_16.py
@@ -84,3 +84,80 @@ for ticket in nearby_tickets:
             invalid_total += number
 
 print(f"Day 16 task 1: {invalid_total}")
+
+def ticket_is_valid(ticket):
+    ticket_invalid = False
+    for number in ticket:
+        number_invalid = True
+        for rule_name in rules:
+            rule = rules[rule_name]
+            if follows_rule(number, rule):
+                number_invalid = False
+                break
+        if number_invalid:
+            ticket_invalid = True
+            break
+    return not ticket_invalid
+
+valid_nearby_tickets = list(filter(ticket_is_valid, nearby_tickets))
+
+# Figure out which rules match all tickets in each ticket field.
+valid_rules = {}
+number_of_fields = len(valid_nearby_tickets[0])
+for i in range(number_of_fields):
+    for rule_name in rules:
+        rule = rules[rule_name]
+        valid_rule = True
+        for ticket in valid_nearby_tickets:
+            number = ticket[i]
+            if not follows_rule(number, rule):
+                valid_rule = False
+                break
+        if valid_rule:
+            rule_list = valid_rules.get(i, [])
+            rule_list.append(rule_name)
+            valid_rules[i] = rule_list
+
+print("valid rule map:")
+print(valid_rules)
+
+# Reduce map to final map
+rule_field_map = {}
+while len(valid_rules) > 0:
+    # Find the field with only one rule
+    next_field = None
+    for field_i in valid_rules:
+        if len(valid_rules[field_i]) == 1:
+            next_field = field_i
+            break
+    if next_field is None:
+        raise RuntimeError("Weird situation!")
+
+    next_rule_name = valid_rules[next_field][0]
+    rule_field_map[next_rule_name] = next_field
+
+    # Clean the valid_rules map
+    for i in range(number_of_fields):
+        if i in valid_rules:
+            rules_list = valid_rules[i]
+            try:
+                idx = rules_list.index(next_rule_name)
+            except:
+                continue
+            del rules_list[idx]
+            if len(rules_list) == 0:
+                del valid_rules[i]
+            else:
+                valid_rules[i] = rules_list
+
+print("field map:")
+print(rule_field_map)
+
+part_2_sol = 1
+starts_with = 'departure'
+for rule_name in rule_field_map:
+    if rule_name[:len(starts_with)] == starts_with:
+        part_2_sol *= my_ticket[rule_field_map[rule_name]]
+
+print(my_ticket)
+print(f"Day 16 part 2: {part_2_sol}")
